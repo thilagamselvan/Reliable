0baf73ce138fed52c314d409499ecc921f2012e4
thilagams <thilagamselvan888@gmail.com>
2023-09-28 12:33:06 +0530

    Demo project created

---
+++ src/test/java/org/demo/Firstgit.java
@@ -0,0 +1,19 @@
+package org.demo;
+
+public class Firstgit {
+	
+	public void text(){
+        System.out.println("Welcome to Reliable");
+		System.out.println("First line");
+		System.out.println("Second line");
+		System.out.println("Third line");
+		System.out.println("Fourth line");
+
+		}
+	
+	public static void main(String[] args){
+		Firstgit f = new Firstgit();
+		f.text();
+	
+}
+}
